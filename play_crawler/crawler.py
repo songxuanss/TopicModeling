b99ab3e5a7e47e819c9e869dd3a7e6aac2a1c37e
songxuanss <songxuanss@hotmail.com>
2015-03-17 19:45:53 +0700

    Added:    1. dictionary based on more than 15 apks on google market.    2. topic model based on those apks.

--- play_crawler/crawler.py
+++ play_crawler/crawler.py
@@ -15,68 +15,46 @@ import json
 
 __author__ = 'pauls'
 
+
 class PlayCrawler:
-    def __init__(self):
-        pass
+	def __init__(self):
+		self.__top100free__ = 'https://play.google.com/store/apps/collection/topselling_free?hl=en&num=%s'
+		self.__top10000free__ = 'https://play.google.com/store/apps/collection/topselling_free?hl=en&authuser=0'
+		self.__apkdetail__ = 'https://play.google.com/store/apps/details?id=%s&hl=en'
 
-    def getTopFree(self, num):
-        print "getting apk from google site..."
+	def getTop100Free(self, num):
+		print "getting apk from google site..."
+		url = self.__top100free__
+		h = httplib2.Http()
+		(resp, content) = h.request(url % num, 'GET')
+		rs = self.parser(content)
+		print "finish getting apk from google site"
+		return rs
 
-        url = 'https://play.google.com/store/apps/collection/topselling_free?hl=en&num=%s'
-        h = httplib2.Http()
-        (resp, content) = h.request(url % num, 'GET')
+	def parser(self, result):
+		soup = BeautifulSoup(result)
+		apklist = soup.find_all('div', {'data-docid': True, 'class': 'card no-rationale square-cover apps small'})
+		apks = []
 
-        rs = self.parser(content)
-        print "finish getting apk from google site"
-        return rs
+		for apk in apklist:
+			# Parse version infomation
+			h = httplib2.Http()
+			sub_url = self.__apkdetail__
+			(resp, content) = h.request(sub_url % apk['data-docid'], 'GET')
+			version = self.__getVersion(content)
+			apks.append('\t'.join([apk['data-docid'], version.get_text().strip() if version is not None else '']))
+		return apks
 
-    def parser(self, result):
-        soup = BeautifulSoup(result)
-        apklist = soup.find_all('div', {'data-docid':True, 'class':'card no-rationale square-cover apps small'})
-        apks = []
-
-        for apk in apklist:
-            # Parse version infomation
-            h = httplib2.Http()
-            sub_url = 'https://play.google.com/store/apps/details?id=%s&hl=en'
-            (resp, content) = h.request(sub_url % apk['data-docid'], 'GET')
-            version = self.getVersion(content)
-            apks.append('\t'.join([apk['data-docid'], version.get_text().strip() if version is not None else '']))
-        return apks
-
-    def getVersion(self, content):
-        sub_soup = BeautifulSoup(content)
-        version = sub_soup.find('div', {'class':'content', 'itemprop':'softwareVersion'})
-        return version
-
-class WandoujiaCrawler():
-
-    def __init__(self):
-        pass
-
-    def getTopFree(self, num):
-        apks = []
-        i = 0
-        while num / 20 > 0:
-            num = num - 20
-            apks.extend(self.getApks(i, 20))
-            i = i + 20
-        if num % 20 > 0:
-            apks.extend(self.getApks(i, num % 20))
-        return apks
-
-    def getApks(self, start, count):
-        url = 'http://apps.wandoujia.com/api/v1/apps?\
-        type=weeklytopapp&max=%s&start=%s&opt_fields=stat.weeklyStr,likesCount,reason,ad,title,\
-        packageName,apks.size,icons.px68,apks.superior,installedCountStr,snippet,apks.versionCode,tags.*&_=1394082542549'
-        h = httplib2.Http()
-        (resp, content) = h.request(url % (count, 0), "GET",
-                                    headers={ 'Content-Type': 'text/javascript', 'charset': 'UTF-8'})
-        res = json.loads(content)
-        apks = []
-        for item in res:
-            apks.append(item['packageName'])
-
-        return apks
+	def __getVersion(self, content):
+		assert isinstance(content, str)
+		sub_soup = BeautifulSoup(content)
+		version = sub_soup.find('div', {'class': 'content', 'itemprop': 'softwareVersion'})
+		return version
 
 
+
+if __name__ == '__main__':
+	p = PlayCrawler()
+	rs = p.getTopFree(100)
+	apklist = p.parser(rs)
+	print apklist
